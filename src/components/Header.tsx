43986e6968b00150fcb5769046bf52e72067c1c8
BlueVelocity <josephrodney@hotmail.com>
2024-08-13 22:46:19 -0400

    Display header

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <header className="w-full py-2 my-2 rounded bg-[#2f6dbd] text-center">
+      <p className="flex-1 text-3xl text-yellow-400 font-bold">
+        P*kemon Matching
+      </p>
+    </header>
+  );
+}
