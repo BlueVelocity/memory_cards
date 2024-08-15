9f4ce99b7ea626cfaa8f931966e628c04edf4228
BlueVelocity <josephrodney@hotmail.com>
2024-08-15 18:24:10 -0400

    Hard code pokemon generation ids
    Allows for quicker loading due to less fetch requests,
    as well as avoiding the need to determine if a name is valid

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -7,38 +7,30 @@ interface PokeData {
   url: string,
 }
 
+const genIdRanges: { [index: string]: Array<number> } = {
+  "1": [1, 151],
+  "3": [252, 386],
+  "5": [494, 649],
+}
+
 export default function Game() {
   const [tileCount, setTileCount] = useState(10);
   const [generation, setGeneration] = useState("1");
   const [pokeData, setPokeData] = useState<Array<PokeData>>([]);
-  const [genData, setGenData] = useState<{ [index: string]: any } | null>();
 
   const getPokemonData = async () => {
-    let tempGenData: { [index: string]: any } | null | undefined = genData;
-    if (!genData) {
-      const gen1Res = await fetch(`https://pokeapi.co/api/v2/generation/1/`);
-      const gen3Res = await fetch(`https://pokeapi.co/api/v2/generation/3/`);
-      const gen5Res = await fetch(`https://pokeapi.co/api/v2/generation/5/`);
-      const gen1Data = await gen1Res.json();
-      const gen3Data = await gen3Res.json();
-      const gen5Data = await gen5Res.json();
-
-      setGenData(() => ({ "1": gen1Data, "3": gen3Data, "5": gen5Data }));
-      tempGenData = { "1": gen1Data, "3": gen3Data, "5": gen5Data };
-    }
-
     // Randomly generate numbers to correspond with pokemon
     const randNums: Array<number> = [];
-    for (let i = 0; i < 10; i++) {
-      const randNum = Math.floor(Math.random() * (tempGenData![`${generation}`].pokemon_species.length - 1));
+    for (let i = 0; i < tileCount; i++) {
+      const randNum = Math.floor(Math.random() * (genIdRanges[generation][1] - genIdRanges[generation][0] + 1)) + genIdRanges[generation][0];
       randNums.includes(randNum) ? i-- : randNums.push(randNum);
     }
 
-    const selectedPokemon: Array<PokeData> = [];
     const getSelectedPokemon = async () => {
+      const selectedPokemon: Array<PokeData> = [];
       for (const num of randNums) {
-        const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${tempGenData ? tempGenData[`${generation}`].pokemon_species[num].name : "1"}/`)
-        const pokemonData = await pokemon.json();
+        const pokemonResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${num}/`)
+        const pokemonData = await pokemonResponse.json();
         const name = pokemonData.name.toUpperCase();
 
         selectedPokemon.push({ name: name, url: pokemonData.sprites.front_default })
@@ -52,7 +44,7 @@ export default function Game() {
 
   useEffect(() => {
     getPokemonData();
-  }, [generation])
+  }, [tileCount, generation])
 
   const handleDifficultyChange = (e: any) => {
     setTileCount(Number(e.target.name));
