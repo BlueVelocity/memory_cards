8a07008f29a8920ec3c4194cdbb05c7f1f5b4f63
BlueVelocity <josephrodney@hotmail.com>
2024-08-15 20:02:27 -0400

    Change title

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ export default function Header() {
   return (
     <header className="w-full py-2 my-2 rounded bg-[#2f6dbd] text-center">
       <p className="flex-1 text-3xl text-yellow-400 font-bold">
-        P*kemon Matching
+        P*kemon Memory
       </p>
     </header>
   );
